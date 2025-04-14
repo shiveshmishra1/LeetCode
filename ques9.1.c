6c6d3d0cbc7f7a4f1287b02f30dcaf558773e09d
Shivesh Kumar Mishra <shivesh9919052488@gmail.com>
2025-04-14 22:49:04 +0530

    busy

---
+++ ques9.1.c
@@ -0,0 +1,27 @@
+bool isPalindrome(int x) {
+    // Negative numbers are never palindromes
+    if (x < 0) {
+        return false;
+    }
+    
+    // Single digit numbers are always palindromes
+    if (x >= 0 && x < 10) {
+        return true;
+    }
+    
+    // If the number ends with 0, it can't be a palindrome unless it's 0 itself
+    if (x % 10 == 0 && x != 0) {
+        return false;
+    }
+    
+    // Reverse the number and compare
+    long reversed = 0;
+    int original = x;
+    
+    while (x > 0) {
+        reversed = reversed * 10 + x % 10;
+        x /= 10;
+    }
+    
+    return original == reversed;
+}
