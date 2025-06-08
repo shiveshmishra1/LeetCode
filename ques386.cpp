4dc059d819d9ae024af053e20637e5661ec10bb9
Shivesh Kumar Mishra <shivesh9919052488@gmail.com>
2025-06-08 21:41:04 +0530

    lexicographical numbers

---
+++ ques386.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    void dfs(int curr,int& n,vector<int>& ans){
+        if(curr>n)  return;
+
+        ans.push_back(curr);
+        for(int i=0;i<=9;++i)//Not MSD
+            dfs(curr*10+i,n,ans);
+    }
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for(int i=1;i<=9;++i){//Most significant digit
+            dfs(i,n,ans);
+        }
+        return ans;
+    }
+};
