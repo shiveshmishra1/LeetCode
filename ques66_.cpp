e6e6d11524493aef6cb14d2e856f968c3eef6e03
Shivesh Kumar Mishra <shivesh9919052488@gmail.com>
2026-01-01 21:02:30 +0530

    Plus One

---
+++ ques66_.cpp
@@ -0,0 +1,8 @@
+class Solution {
+ public:
+  vector<int> plusOne(vector<int>& digits) {
+    for (int i = digits.size() - 1; i >= 0; --i) {
+      if (digits[i] < 9) {
+        ++digits[i];
+        return digits;
+      }
